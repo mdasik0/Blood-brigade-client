32dcae4036540cb09357f691b1bc2a3b25f145e6
mdasik0 <asikthe1st@gmail.com>
2023-11-26 23:49:33 +0600

    main layout setup complete

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/MainLayout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
   },
 ]);
 
