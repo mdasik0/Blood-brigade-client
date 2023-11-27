cf6ca86a58969e0028bcfa3071431617b4da4626
mdasik0 <asikthe1st@gmail.com>
2023-11-27 09:16:59 +0600

    complete

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -2,8 +2,16 @@
 const Navbar = () => {
     return (
         <>
-            <div>
-                desktop Navbar
+            <div className=" p-3">
+                <div>
+                    <div className="flex items-center text-red-500 h-fit">
+                        <span className="text-[58px] font-bold">B</span>
+                        <div>
+                            <p className="font-bold leading-5 mt-[6px]">lood</p>
+                            <p className="font-semibold pb-1">rigade</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 Other Navbar
