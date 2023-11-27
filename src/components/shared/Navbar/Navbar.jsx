bc79773596b0ea9ff9d23fa42e3bc7380c9defa2
mdasik0 <asikthe1st@gmail.com>
2023-11-27 09:22:26 +0600

    desktop initial navbar setup done(still needs work)

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@
 const Navbar = () => {
     return (
         <>
-            <div className=" p-3">
-                <div>
+            <div className="max-w-[85%] mx-auto ">
+                <div className="flex justify-between items-center">
                     <div className="flex items-center text-red-500 h-fit">
                         <span className="text-[58px] font-bold">B</span>
                         <div>
@@ -11,6 +11,11 @@ const Navbar = () => {
                             <p className="font-semibold pb-1">rigade</p>
                         </div>
                     </div>
+                    <ul className="flex gap-3 font-semibold">
+                        <li className="active bg-red-500 px-3 py-1 rounded-full text-white">Home</li>
+                        <li>Donars</li>
+                        <li>Sign In</li>
+                    </ul>
                 </div>
             </div>
             <div>
