c8c29101a7df4d00b87770364f84698c2b2462cf
mdasik0 <asikthe1st@gmail.com>
2023-11-27 19:01:11 +0600

    small animation practice

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 
 const HomePage = () => {
-    return (
-        <div>
-            This is the homepage
-        </div>
-    );
+  const [circle, setCircle] = useState(false);
+  return (
+    <div className="bg-stone-900 h-screen text-white flex justify-center items-center">
+      <div className="bg-white rounded-xl overflow-hidden h-[500px] relative w-[300px]">
+        <div
+          onClick={() => setCircle(!circle)}
+          className={`bg-blue-400 h-36 w-36 left-20 absolute duration-300   ${
+            circle ? "scale-150 -top-16 rounded-full " : "top-16  rounded-xl"
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
